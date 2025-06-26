b92e9120cd2a031e003ad6c11096496782b16f1e
Shaharyar <shaharyar4t4@gmail.com>
2025-06-27 01:25:03 +0500

    updated

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const Color bg_up = Color(0xFF00d1a0);
 const Color bg_down = Color(0xFF029e7a);
 const Color gt_btn = Color(0xFFFFFFFF);
+const Color w_text = Color(0xFFFFFFFF);
--- lib/feacture/auth/view/view_loginscreen.dart
+++ lib/feacture/auth/view/view_loginscreen.dart
@@ -9,6 +9,8 @@ class ViewLoginscreen extends StatefulWidget {
 }
 
 class _ViewLoginscreenState extends State<ViewLoginscreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +22,123 @@ class _ViewLoginscreenState extends State<ViewLoginscreen> {
             colors: [
               bg_up,
               bg_down,
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 50),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_back_ios_new, color: Colors.black),
+                ),
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/main_log.png', // Replace with your logo asset
+                      height: 80,
+                    ),
+                    const SizedBox(height: 70),
+                    Container(
+                      padding: EdgeInsets.all(24),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Email Address',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Enter your email address, we just sent OTP to your email to register your account.',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                          SizedBox(height: 24),
+                          TextField(
+                            controller: emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.email_outlined),
+                              hintText: 'Enter your email',
+                              filled: true,
+                              fillColor: Colors.grey[200],
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 24),
+                          TextField(
+                            controller: passwordController,
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.password_sharp),
+                              hintText: 'Enter your Password',
+                              filled: true,
+                              fillColor: Colors.grey[200],
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 24),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Your register logic goes here
+                            },
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 90, vertical: 14),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(24),
+                              ),
+                              backgroundColor: bg_down,
+                            ),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  'Register',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.white),
+                                ),
+                                SizedBox(width: 8),
+                                Icon(
+                                  Icons.arrow_forward,
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
